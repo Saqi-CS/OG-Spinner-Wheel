cec2d8d2e571eca0b4bb901e09aca3123b66a942
Bilal Basharat <bilalbisharat@gmail.com>
2022-10-01 16:31:00 +0500

    Intro Page created

--- OG Spinner/Program.cs
+++ OG Spinner/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OG_Spinner.Forms;
 
 namespace OG_Spinner
 {
@@ -16,7 +17,7 @@ namespace OG_Spinner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new IntroFrm());
         }
     }
 }
